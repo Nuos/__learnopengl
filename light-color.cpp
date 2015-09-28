82c7f29f9a3e5df4164cc1ded1a4c7fd89a6c2be
nuos <Gaohuancai@gmail.com>
2015-09-28 12:45:58 +0800

    Update light-color.cpp

--- light-color.cpp
+++ light-color.cpp
@@ -113,6 +113,47 @@ int Color::enter()
     // OpenGL options
     glEnable(GL_DEPTH_TEST);
 
+    /**********************************************************************************
+     * [lighting.vert]
+    #version 330 core
+    layout (location = 0) in vec3 position;
+    uniform mat4 model;
+    uniform mat4 view;
+    uniform mat4 projection;
+    void main()
+    {
+    	gl_Position = projection * view * model * vec4(position, 1.0f);
+    }
+     * [lighting.frag]
+     #version 330 core
+    out vec4 color;
+    uniform vec3 objectColor;
+    uniform vec3 lightColor;
+    void main()
+    {
+    	color = vec4(lightColor * objectColor, 1.0f);
+    }
+     * 
+     * //////////////////////////////////////////////////////////////////
+     * [lamp.vert]
+     #version 330 core
+    layout (location = 0) in vec3 position;
+    uniform mat4 model;
+    uniform mat4 view;
+    uniform mat4 projection;
+    void main()
+    {
+        gl_Position = projection * view * model * vec4(position, 1.0f);
+    } 
+     * 
+     * [lamp.frag]
+     #version 330 core
+    out vec4 color;
+    void main()
+    {
+        color = vec4(1.0f); // Set alle 4 vector values to 1.0f
+    }
+    **********************************************************************************/
     // build and compile our shader program...
     Shader lightingShader("../ShaderSource/lighting.vert", "../ShaderSource/lighting.frag");
     Shader lampShader("../ShaderSource/lamp.vert", "../ShaderSource/lamp.frag");
