9746e15904c15ac1c7fe9f64defb2ceb0cef8227
nuos <Gaohuancai@gmail.com>
2015-09-29 20:36:09 +0800

    Create HelloTriangle.cpp
    glVertexAttribPointer(	GLuint index,GLint size,GLenum type,GLboolean normalized,GLsizei stride,const GLvoid* pointer);
    *0* specifies which vertex attribute we want to configure. Remember that we specified the location of the -position- vertex attribute in the vertex shader with -layout (location = 0)-. This sets the location of the vertex attribute to 0 and since we want to pass data to this vertex attribute, we pass in 0.
    *1* the size of the vertex attribute.
    *2* the type of the data................
    *3* whether we want the data to be normalized...
    *4* the stride and tells us the space between consecutive vertex attribute sets. Since the next set of position data is located exactly 3 times the size of a GLfloat away we specify that value as the stride....
    *5* be of type GLvoid* and thus requires that weird cast....This is the offset of where the position data BEGINS in THE BUFFER.......Since the position data is at the start of THE DATA ARRAY this value is just 0.........
    
    
    void glEnableVertexAttribArray(GLuint index)
    *0* the index of the vertex attribute to be enabled....
    ================================================================================================
    @ initialize the vertex data in a buffer using a vertex-buffer-object,
    @ set up a vertex and fragment shader 
    @ tell OpenGL how to link the vertex data to the vertex shader's vertex attributes..
    ==================================
    // copy vertices-array in a buffer for OpenGL to use
    glBindBuffer( gl_array_buffer, VBO );
    glBufferData( gl_array_buffer, sizeof( vertices ), vertices, gl_static_draw );
    // set the vertex attributes pointers..
    glVertexAttribPointer(0, 3, gl_float, gl_false, 3 * sizeof(GLfloat), (GLvoid*)0);
    glEnableVertexAttribArray(0);
    // use our shader program when we want to render an object
    glUseProgram( shaderProgram );
    // draw the object
    ....

---
+++ HelloTriangle.cpp
@@ -0,0 +1,108 @@
+#include <hellotriangle.h>
+
+#ifdef _HelloTriangle_
+#include <iostream>
+const GLuint WIDTH = 800, HEIGHT = 600;
+
+/* " The vertex shader allows us to specify any input we want in the form of vertex attributes
+ *   and while this allows for great flexibility, 
+ *   it does mean we have to manually specify what part of our input data goes 
+ *   to which vertex attribute in the vertex shader."
+ */
+const size_t locationPos/**/ = 15;// value ranging from [0, 15] is valid.!!!!!!!!!!!!!
+const GLchar* vertexShaderSource =
+"#version 330 core\n\
+//layout (location = 0) in vec3 position;\n\
+layout (location/**/ = 15) in vec3 position;\n\   ///***********************
+void main()\n\
+{\n\
+gl_Position = vec4(position.x, position.y, position.z, 1.0f);\n\
+}\n\0";
+const GLchar* fragmentShaderSource =
+"\n#version 330 core\n\
+out vec4 color;\n\
+void main()\n\
+{\n\
+color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n\
+}\n\0";
+
+
+int HelloTriangle::enter()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL...", nullptr, nullptr);
+    glfwSetKeyCallback(window, key_callback);
+    glfwMakeContextCurrent(window);
+    glewExperimental = GL_TRUE;
+    glewInit();
+    glViewport(0, 0, WIDTH, HEIGHT);
+
+    //////////////////////////////////////////////////////////////////////////
+    // build and compile our shader program
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    GLfloat vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+    GLuint VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    //////////////////////////////////////////////////////////////////////////
+    // register VBO as the currently bound vertex buffer object
+
+    //glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glVertexAttribPointer(locationPos/**/, 3, GL_FLOAT, GL_TRUE, 3 * sizeof(GLfloat), (GLvoid*)0);//*****************
+    //glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(locationPos/**/);//***********************
+    glBindBuffer(GL_ARRAY_BUFFER, 0);// unbind VBO
+    glBindVertexArray(0);// unbind VAO
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+
+        glfwSwapBuffers(window);
+    }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glfwTerminate();
+    return 0;
+}
+
+void HelloTriangle::key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
+
+#endif// _HelloTriangle_
